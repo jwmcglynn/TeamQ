4ded635cd115b060c404bd2e263434d86a192a61
Jared Day <jaredday@jared-days-macbook-pro.local>
2010-11-04 22:32:13 -0700

    Fixed issues with the faction ships.

--- ShipController.cs
+++ ShipController.cs
@@ -7,6 +7,6 @@ namespace Sputnik
 {
     interface ShipController
     {
-        void Update(Ship s, float elapsedTime);
+		void Update(Ship s, float elapsedTime);
     }
 }
---
+++ SquaretopiaShip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+
+namespace Sputnik
+{
+	class SquaretopiaShip : Ship
+	{
+		public SquaretopiaShip(float x, float y, float vx, float vy, float sx, float sy, float fx, float fy, GameEnvironment env)
+			: base(x, y, vx, vy, sx, sy, fx, fy, env)
+		{
+			this.shooter = new BulletEmitter(env, BulletEmitter.BulletStrength.Strong, IsFriendly());
+			env.AddChild(this.shooter);
+
+			this.LoadTexture(env.contentManager, "squaretopia");
+
+			Registration = new Vector2(Texture.Width, Texture.Height) * 0.5f;
+			CreateCollisionBody(env.CollisionWorld, BodyType.Dynamic, CollisionFlags.Default);
+			AddCollisionCircle(Texture.Width * 0.5f, Vector2.Zero);
+			CollisionBody.LinearDamping = 8.0f;
+		}
+	}
+}
