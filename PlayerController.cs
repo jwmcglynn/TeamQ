9b130172d8c90adba251b30282c2233af5f7e6e7
Jeff McGlynn <jmcglynn@uci.edu>
2010-12-01 23:49:22 -0800

    Added xbox controller support and directional mouse input. - Art for directional mouse input needs to be changed to be an arrow instead of a cursor.

--- PlayerController.cs
+++ PlayerController.cs
@@ -17,8 +17,6 @@ namespace Sputnik
     {
 		private GameEnvironment m_env;
 		private const float timeBetweenControls = 0.0f;
-		private bool specialShot = true;
-		private float lastSpace = 0.0f;
 		private BlackHole.Pair m_playerBlackHoles;
 		private bool isTractoringItem;
 		private Entity itemBeingTractored;
@@ -94,22 +92,92 @@ namespace Sputnik
         ///  Updates the State of a ship
         /// </summary>
 
-        public void Update(Ship s, float elapsedTime)
-        {
+        public void Update(Ship s, float elapsedTime) {
+			const float k_aimRadius = 250.0f;
+
 			s.ResetMaxRotVel();
-
 			controlled = s;
-			KeyboardState kb = Keyboard.GetState();
-			MouseState ms = Mouse.GetState();
-			Vector2 mousePos = m_env.Camera.ScreenToWorld(new Vector2(ms.X, ms.Y));
-			s.shooterRotation = Angle.Direction(s.Position, mousePos);
 
-			lastSpace -= elapsedTime;
-			if (lastSpace < 0)
-				lastSpace = 0.0f;
+			GamePadState gamepad = GamePad.GetState(PlayerIndex.One);
 
+			// Get keyboard state.
+			Vector2 movement = Vector2.Zero;
 
-			if (kb.IsKeyDown(Keys.Space) && !OldKeyboard.GetState().IsKeyDown(Keys.Space))
+			Vector2 aimPosition = Vector2.Zero;
+			float aimDirection;
+
+			Vector2 specialPosition = Vector2.Zero;
+			float specialDirection;
+
+			bool detachPressed = false;
+
+			bool useSpecialPressed = false;
+			bool useSpecialHeld = false;
+
+			bool shoot = false;
+
+			if (!gamepad.IsConnected) {
+				KeyboardState keyboard = Keyboard.GetState();
+				MouseState mouse = Mouse.GetState();
+				MouseState oldMouse = OldMouse.GetState();
+
+				if (keyboard.IsKeyDown(Keys.W)) movement.Y -= 1.0f;
+				if (keyboard.IsKeyDown(Keys.A)) movement.X -= 1.0f;
+				if (keyboard.IsKeyDown(Keys.S)) movement.Y += 1.0f;
+				if (keyboard.IsKeyDown(Keys.D)) movement.X += 1.0f;
+
+				// Aiming.  TODO: Direction, not position.
+				Vector2 mousePos = m_env.Camera.ScreenToWorld(new Vector2(mouse.X, mouse.Y)) - m_env.Camera.Position;
+				mousePos /= k_aimRadius;
+				if (mousePos.Length() > 1.0f) mousePos.Normalize();
+				mousePos *= k_aimRadius;
+
+				specialPosition = Vector2.Normalize(mousePos) * k_aimRadius + s.Position;
+				aimDirection = Angle.Direction(Vector2.Zero, mousePos);
+				specialDirection = aimDirection;
+
+				{
+					// Reset mouse position to center.
+					Vector2 screenCenter = m_env.Camera.WorldToScreen(m_env.Camera.Position + mousePos);
+					Mouse.SetPosition((int) screenCenter.X, (int) screenCenter.Y);
+				}
+
+				// Detach.
+				detachPressed = (keyboard.IsKeyDown(Keys.Space) && !OldKeyboard.GetState().IsKeyDown(Keys.Space));
+
+				// Use special.
+				useSpecialHeld = (mouse.RightButton == ButtonState.Pressed && oldMouse.RightButton == ButtonState.Pressed);
+				useSpecialPressed = (mouse.RightButton == ButtonState.Pressed && oldMouse.RightButton != ButtonState.Pressed);
+
+				// Shoot.
+				shoot = (mouse.LeftButton == ButtonState.Pressed);
+
+			} else {
+				GamePadState oldGamepad = OldGamePad.GetState();
+				Vector2 invertY = new Vector2(1.0f, -1.0f);
+
+				// Gamepad.
+				movement = gamepad.ThumbSticks.Left * invertY;
+				aimDirection = Angle.Direction(Vector2.Zero, gamepad.ThumbSticks.Right * k_aimRadius * invertY);
+				specialPosition = Angle.Vector(s.DesiredRotation) * k_aimRadius + s.Position;
+				specialDirection = s.DesiredRotation;
+
+				// Detach.
+				detachPressed = (gamepad.IsButtonDown(Buttons.LeftShoulder) && !oldGamepad.IsButtonDown(Buttons.LeftShoulder))
+									|| (gamepad.IsButtonDown(Buttons.RightShoulder) && !oldGamepad.IsButtonDown(Buttons.RightShoulder));
+
+				// Use special.
+				useSpecialHeld = (gamepad.IsButtonDown(Buttons.A) && oldGamepad.IsButtonDown(Buttons.A));
+				useSpecialPressed = (gamepad.IsButtonDown(Buttons.A) && !oldGamepad.IsButtonDown(Buttons.A));
+
+				// Shoot.
+				shoot = (gamepad.ThumbSticks.Right.Length() > 0.1f);
+			}
+
+			// Act on input.
+			s.shooterRotation = aimDirection;
+
+			if (detachPressed)
 			{
 				s.Detach();
 				// If i am tractoring something, and i detach from it, then they should go back to normal.
@@ -121,32 +189,26 @@ namespace Sputnik
 				}
 			}
 
-			Vector2 temp = Vector2.Zero;
-			if (kb.IsKeyDown(Keys.W)) temp.Y -= 1.0f;
-			if (kb.IsKeyDown(Keys.A)) temp.X -= 1.0f;
-			if (kb.IsKeyDown(Keys.S)) temp.Y += 1.0f;
-			if (kb.IsKeyDown(Keys.D)) temp.X += 1.0f;
-			s.DesiredVelocity = (temp != Vector2.Zero) ? Vector2.Normalize(temp) * s.maxSpeed : Vector2.Zero;
+			s.DesiredVelocity = (movement != Vector2.Zero) ? Vector2.Normalize(movement) * s.maxSpeed : Vector2.Zero;
 			if (s.DesiredVelocity != Vector2.Zero) {
 				s.DesiredRotation = Angle.Direction(Vector2.Zero, s.DesiredVelocity);
 			}
 
 			// need to check if sputnik is in a ship or not before you can shoot.
-			if (ms.LeftButton == ButtonState.Pressed)
-				s.Shoot(elapsedTime);
+			if (shoot) s.Shoot(elapsedTime);
 
 			// Will spawn a blackhole when we first pressdown our right mouse button.
 			// if a blackhole has already been spawned this way, then the other one will be removed.
-			if(ms.RightButton == ButtonState.Pressed && !specialShot) {
+			if(useSpecialPressed) { // See useSpecialHeld for moving the blackhole.
 				if(s is CircloidShip) {
 					if (m_playerBlackHoles != null) m_playerBlackHoles.Destroy();
-					m_playerBlackHoles = BlackHole.CreatePair(m_env, m_env.Camera.ScreenToWorld(new Vector2(ms.X, ms.Y)));
+					m_playerBlackHoles = BlackHole.CreatePair(m_env, specialPosition);
 				} else if(s is TriangulusShip) {
 					
 					// if we are tractoring something right now, then we arent allowed to tractor anything else
 					// we can shoot now.
 					if(!isTractoringItem) {
-						List<Entity> list = VisionHelper.FindAll(m_env, s.Position, s.shooterRotation, MathHelper.ToRadians(20.0f), 500.0f);
+						List<Entity> list = VisionHelper.FindAll(m_env, s.Position, specialDirection, MathHelper.ToRadians(20.0f), 500.0f);
 						IOrderedEnumerable<Entity> sortedList = list.OrderBy(ent => Vector2.DistanceSquared(s.Position, ent.Position)); 
 
 						Entity collided = sortedList.FirstOrDefault(ent =>
@@ -181,29 +243,9 @@ namespace Sputnik
 						isTractoringItem = false;
 					}
 				} else if(s is SquaretopiaShip) {
-					ForceField ff = new ForceField(m_env, s.Position, s.shooterRotation, controlled);
+					ForceField ff = new ForceField(m_env, s.Position, specialDirection, controlled);
 					m_env.AddChild(ff);
 				}
-
-				specialShot = true;
-			}
-			if(ms.RightButton == ButtonState.Released) {
-				specialShot = false;
-			}
-
-			// Debug test for VisionHelper.
-			if (kb.IsKeyDown(Keys.Q) && !OldKeyboard.GetState().IsKeyDown(Keys.Q)) {
-				List<Entity> list = VisionHelper.FindAll(m_env, s.Position, s.shooterRotation, MathHelper.ToRadians(20.0f), 2000.0f);
-				IOrderedEnumerable<Entity> sortedList = list.OrderBy(ent => Vector2.DistanceSquared(s.Position, ent.Position));
-
-				Entity collided = sortedList.FirstOrDefault(ent => {
-					if (ent is Ship && controlled.IsFriendly((Ship)ent)) return false;
-					else if (ent is Boss && controlled.IsFriendly((Boss)ent)) return false;
-					if (ent is Environment) return false;
-					return true;
-				});
-
-				if (collided != null) collided.Dispose();
 			}
 		}
     }
