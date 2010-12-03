7b804ad2bad1fffd7b3d6a1c9f8cf6612a28462c
Jeff McGlynn <jmcglynn@uci.edu>
2010-12-02 18:13:41 -0800

    Added level 1 from Kayu.

---
+++ Level1Environment.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Collision.Shapes;
+
+namespace Sputnik {
+	class Level1Environment : GameEnvironment {
+		public Level1Environment(Controller ctrl)
+			: base(ctrl) {
+
+			LoadMap("Level_1.tmx");
+			Sound.PlayCue("music");
+		}
+	}
+}
